69faf57f7015252a1ff9cb3f8e6adadeacd19900
Vishruth Khatri <itsvishruthkhatri@hotmail.com>
2022-11-30 22:16:02 -0500

    Update urls.py

--- Ashish08/urls.py
+++ Ashish08/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.views.static import serve
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -33,5 +34,13 @@ urlpatterns = [
 urlpatterns = urlpatterns+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
 
+
+favicon_view = RedirectView.as_view(url='/static/favicon.ico', permanent=True)
+
+urlpatterns = [
+    ...
+    re_path(r'^favicon\.ico$', favicon_view),
+    ...
+]
 
 
